36122d42008a8d7c78d2b34091a96b3f6bba0569
Justin Meyer <justin.meyer@dpi.wi.gov>
2018-02-24 17:50:41 -0600

    App allows user to enter text and produces "test text" output.

--- word_predictor/ui.R
+++ word_predictor/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -18,11 +9,6 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30),
        textInput("text_input",
                  "Enter text here:",
                  placeholder = "Enter text here")
@@ -30,7 +16,7 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       textOutput("output_text")
     )
   )
 ))
